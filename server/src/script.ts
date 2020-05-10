95dfc9d6d6f8c8c7bbc581cda2c90f6ed137caa2
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-05-10 01:23:27 +0100

    initial commit

---
+++ server/src/script.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+  const allUsers = await prisma.user.findMany();
+
+  console.log(allUsers);
+}
+
+main()
+  .catch(e => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
