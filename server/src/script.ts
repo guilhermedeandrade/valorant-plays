0a572a49cba7d842337647f0547e8d6796dc54e1
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-05-10 02:58:23 +0100

    chore: remove deletedAt field and improve updatedAt field from User model

--- server/src/script.ts
+++ server/src/script.ts
@@ -3,6 +3,29 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main(): Promise<void> {
+  await prisma.user.create({
+    data: {
+      email: 'foo@email.com',
+      username: 'foo',
+      password: 'kekw',
+    },
+  });
+
+  await prisma.play.create({
+    data: {
+      map: 'Bind',
+      agent: 'Sova',
+      usage: 'Offensive',
+      title: 'Lorem Ipsum Arrow',
+      gifUrl: 'https://gif-url',
+      detailedImageUrl: 'https://detailed-img-url',
+      submitter: {
+        connect: { username: 'foo' },
+      },
+      description: 'op arrow pls nerf',
+    },
+  });
+
   const allUsers = await prisma.user.findMany();
   const allPlays = await prisma.play.findMany();
 
