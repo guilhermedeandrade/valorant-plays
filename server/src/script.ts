c9a6951f3735ee5f7badd6a2ab35dffd54dfc776
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2020-05-10 02:48:50 +0100

    feat: create Play model

--- server/src/script.ts
+++ server/src/script.ts
@@ -4,8 +4,10 @@ const prisma = new PrismaClient();
 
 async function main(): Promise<void> {
   const allUsers = await prisma.user.findMany();
+  const allPlays = await prisma.play.findMany();
 
   console.log(allUsers);
+  console.log(allPlays);
 }
 
 main()
